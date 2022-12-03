377cf74e65808e5a1b8e679b9b15d3eef4eb94f0
Gresh Basic <100721569+greshbasic@users.noreply.github.com>
2022-12-02 18:22:00 -0600

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from threading import Timer
-
+'''
 x=datetime.today()
 y = x.replace(day=x.day, hour=1, minute=0, second=0, microsecond=0) + timedelta(days=1)
 delta_t=y-x
@@ -13,3 +13,23 @@ async def semantle_reminder():
 
 t = Timer(secs, semantle_reminder)
 t.start()
+'''
+from datetime import datetime
+import threading
+
+'''
+version 2
+'''
+async def checkTime():
+    threading.Timer(1, checkTime).start()
+
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    if(current_time == '06:00:00'):
+        await ctx.send("@everyone its semantle time!"
+
+
+checkTime()
