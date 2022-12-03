ac620083091dd860aa1ca6845642489948e611f8
Gresh Basic <100721569+greshbasic@users.noreply.github.com>
2022-12-02 18:14:17 -0600

    Create reminder.py

---
+++ reminder.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from threading import Timer
+
+x=datetime.today()
+y = x.replace(day=x.day, hour=1, minute=0, second=0, microsecond=0) + timedelta(days=1)
+delta_t=y-x
+
+secs=delta_t.total_seconds()
+
+async def semantle_reminder():
+    await ctx.send("hello world")
+    
+
+t = Timer(secs, hello_world)
+t.start()
